d5c147f1d5175a2ab0fe5328736e8f6234f40f74
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 <savshev@gmail.com>
2026-09-18 09:56:57 +0300

    Fix Veterans menu button route link

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -43,7 +43,12 @@
     <div class="menu-grid">
         <!-- 8 нових розділів -->
         <a href="{{ route('working') }}" class="menu-btn">Працюючі</a>
-        <a href="#" class="menu-btn">Ветерани</a>
+{{--        <a href="#" class="menu-btn">Ветерани</a>--}}
+        <!-- Кнопка Ветераны -->
+        <a href="{{ route('veterans') }}" class="menu-btn interactive-btn">
+            <span>Ветерани</span>
+        </a>
+
         <a href="#" class="menu-btn">Підрозділи</a>
         <a href="#" class="menu-btn">Путівки</a>
         <a href="#" class="menu-btn">Фіндопомога</a>
@@ -53,11 +58,6 @@
     </div>
 
     <a href="{{ route('main') }}" class="btn-back">← Назад</a>
-
-    <!-- Кнопка Ветераны -->
-    <a href="{{ route('veterans') }}" class="menu-btn interactive-btn">
-        <span>Ветерани</span>
-    </a>
 </div>
 </body>
 </html>
